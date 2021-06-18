d929744989d65dca75f19653af0e74800adf149c
Simone Taeggi <startae14@gmail.com>
2021-06-18 09:41:55 +0200

    MAIN 06: use HashRouter instead of BrowserRouter react-router component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { HashRouter, Route, Link } from 'react-router-dom';
 
 const PageOne = () => (
   <div>
@@ -19,12 +19,12 @@ const PageTwo = () => (
 const App = () => {
   return (
     <div>
-      <BrowserRouter>
+      <HashRouter>
         <div>
           <Route path="/" exact component={PageOne} />
           <Route path="/pagetwo" component={PageTwo} />
         </div>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 };
